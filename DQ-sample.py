67847e956fcdf8b78194e06526c5fbdd1d5755f2
data-mavericks-2024 <163488974+data-mavericks-2024@users.noreply.github.com>
2025-01-22 15:08:07 +0000

    dd

--- DQ-sample.py
+++ DQ-sample.py
@@ -1,29 +1,39 @@
-
+# Import required modules from GX library.
 import great_expectations as gx
 
+import pandas as pd
+
+# Create Data Context.
 context = gx.get_context()
 
-
-
-# Use the `pandas_default` Data Source to retrieve a Batch of sample Data from a data file:
-file_path = "./pharma_data.csv"
-batch = context.data_sources.pandas_default.read_csv(file_path)
-
-# Define the Expectation to test:
-expectation = gx.expectations.ExpectColumnMaxToBeBetween(
-    column="sales_amount", min_value=1, max_value=6000
+# Import sample data into Pandas DataFrame.
+df = pd.read_csv(
+    "./pharma_data.csv"
 )
 
-# Test the Expectation:
-validation_results = batch.validate(expectation)
+# Connect to data.
+# Create Data Source, Data Asset, Batch Definition, and Batch.
+data_source = context.data_sources.add_pandas("pandas")
+data_asset = data_source.add_dataframe_asset(name="pd dataframe asset")
+
+batch_definition = data_asset.add_batch_definition_whole_dataframe("batch definition")
+batch = batch_definition.get_batch(batch_parameters={"dataframe": df})
+
+# Create Expectation.
+expectation_region = gx.expectations.ExpectColumnValuesToBeInSet(
+    column="region", value_set=["North", "South", "East", "West"],
+)
+
+expectation = gx.expectations.ExpectColumnValuesToBeBetween(
+    column="sales_amount", min_value=1, max_value=6
+)
+
+# Validate Batch using Expectation.
+validation_result = batch.validate(expectation)
+validation_result1 = batch.validate(expectation_region)
+
+
 
 # Evaluate the Validation Results:
-print(validation_results)
-
-# If needed, adjust the Expectation's preset parameters and test again:
-# expectation.min_value = 1
-# expectation.max_value = 60000
-
-# # Test the modified expectation and review the new Validation Results:
-# new_validation_results = batch.validate(expectation)
-# print(new_validation_results)
+print(validation_result)
+print(validation_result1)
